5d5dc158f59959bf41d27db8929b4f56793a3278
Julio Marroquin <soulx963@gmail.com>
2025-10-04 19:57:50 -0500

    Installed FireBase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4J8YJ9BhVZVmP9Yr1oYoF9P3AC2b_lGI",
+  authDomain: "homework5-e44f7.firebaseapp.com",
+  projectId: "homework5-e44f7",
+  storageBucket: "homework5-e44f7.firebasestorage.app",
+  messagingSenderId: "784560688089",
+  appId: "1:784560688089:web:a6c49b4fa4e79994cd27c8"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
